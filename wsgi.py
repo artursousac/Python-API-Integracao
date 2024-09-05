15acfc78281f6e4cb8b00208582f72b8209a13e8
artursousac <artursousac@hotmail.com>
2024-09-04 23:56:33 -0300

    .

---
+++ wsgi.py
@@ -0,0 +1,19 @@
+import os, sys
+
+virtenv = os.path.expanduser('~') + '/venv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    if sys.version.split(' ')[0].split('.')[0] == '3':
+        exec(compile(open(virtualenv, "rb").read(), virtualenv, 'exec'), dict(__file__=virtualenv))
+    else:
+        exec(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+
+sys.path.append(os.path.expanduser('~'))
+sys.path.append(os.path.expanduser('~') + '/ROOT/')
+
+from app import app
+from a2wsgi import ASGIMiddleware
+
+application = ASGIMiddleware(app.app)
